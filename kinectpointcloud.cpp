b44db45c76a383443ee05f02c03df42535bffc4c
bkappel <bart.kappel@gmail.com>
2013-05-26 07:37:19 -0700

    update for kinect pointcloud from openni ros topic

--- kinectpointcloud.cpp
+++ kinectpointcloud.cpp
@@ -2,12 +2,93 @@
 #include <pcl/ModelCoefficients.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 
-int main(int argc, char** argv) {
+
+sensor_msgs::PointCloud2 kinectcloud;
+ros::Publisher visionPublisher;
+
+// Ros kinect topic callback
+void callback(const sensor_msgs::PointCloud2ConstPtr& input) {
+	//Inside the callback should be all the process that you want to do with your point cloud and at the end publish the results.
+	printf("Before filtering Cloud: width = %d, height = %d\n", input->width,
+			input->height);
+
+	sensor_msgs::PointCloud2 voxeloutput;
+
+	//Down sizing the cloud because of the real time performance
+	pcl::VoxelGrid<sensor_msgs::PointCloud2> sor;
+	sor.setInputCloud (input);
+	sor.setLeafSize (0.01f, 0.01f, 0.01f);
+	sor.filter (voxeloutput);
+
+	std_msgs::String msg;
+	std::stringstream ss;
+	ss << "width " << voxeloutput.width << "\n";
+	ss << "height " << voxeloutput.height << "\n";
+	msg.data = ss.str();
+	visionPublisher.publish(msg);
+
+	/*pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+	pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+	pcl::SACSegmentation<pcl::PointXYZ> seg;
+	seg.setOptimizeCoefficients(true);
+	seg.setModelType(pcl::SACMODEL_PLANE);
+	seg.setMethodType(pcl::SAC_RANSAC);
+	seg.setDistanceThreshold(0.01);
+
+	//seg.setInputCloud(cloud.makeShared());
+	seg.setInputCloud(voxeloutput);
+	seg.segment(*inliers, *coefficients);
+
+	if (inliers->indices.size() == 0) {
+		PCL_ERROR("Could not estimate a planar model for the given dataset.");
+		return;
+	}
+
+	std::cerr << "Model coefficients: " << coefficients->values[0] << " "
+			<< coefficients->values[1] << " " << coefficients->values[2] << " "
+			<< coefficients->values[3] << std::endl;
+
+	std::cerr << "Model inliers: " << inliers->indices.size() << std::endl;
+	for (size_t i = 0; i < inliers->indices.size(); ++i)
+		std::cerr << inliers->indices[i] << "    "
+				<< cloud.points[inliers->indices[i]].x << " "
+				<< cloud.points[inliers->indices[i]].y << " "
+				<< cloud.points[inliers->indices[i]].z << std::endl;
+
+	// Do some processing to the point cloud
+	/*pcl::VoxelGrid<sensor_msgs::PointCloud2> sor;
+	 sor.setInputCloud (input);
+	 sor.setLeafSize (0.01f, 0.01f, 0.01f);
+	 sor.filter (output);
+	 printf ("After filtering Cloud: width = %d, height = %d\n", output.width, output.height);
+
+	 //Publish the results
+	 pub.publish(output);*/
+}
+
+int main(int argc, char** argv)
+{
+	ros::init (argc, argv, "SUB_IND_PUB");
+	ros::NodeHandle nh;
+	visionPublisher = nh.advertise<std_msgs::String>("/vision/grabbing/location", 1000);
+
+	//ros::Subscriber sub = nh.subscribe<pcl::PointCloud>("/camera/depth_registered/points", 1, callback);
+	ros::Subscriber sub = nh.subscribe("/camera/depth_registered/points", 1, callback);
+
+	  //This will run until you shutdown the node
+	ros::spin();
+}
+
+/*int main(int argc, char** argv) {
+
 	pcl::PointCloud<pcl::PointXYZ> cloud;
 
 	// Fill in the cloud data
@@ -20,18 +101,18 @@ int main(int argc, char** argv) {
 		//cloud.points[i].x = 1024 * rand() / (RAND_MAX + 1.0f);
 		cloud.points[i].x = 1.0;
 		//cloud.points[i].y = 1024 * rand() / (RAND_MAX + 1.0f);
-		cloud.points[i].y= 1.0;
+		cloud.points[i].y = 1.0;
 		//cloud.points[i].z = 1.0;
 		cloud.points[i].z = 1024 * rand() / (RAND_MAX + 1.0f);
 	}
 
 	for (size_t i = cloud.points.size() / 2; i < cloud.points.size(); ++i) {
 		//cloud.points[i].x = 1024 * rand() / (RAND_MAX + 1.0f);
-				cloud.points[i].x = 2.0;
-				cloud.points[i].y = 1024 * rand() / (RAND_MAX + 1.0f);
-				//cloud.points[i].y= 2.0;
-				//cloud.points[i].z = 2.0;
-				cloud.points[i].z = 1024 * rand() / (RAND_MAX + 1.0f);
+		cloud.points[i].x = 2.0;
+		cloud.points[i].y = 1024 * rand() / (RAND_MAX + 1.0f);
+		//cloud.points[i].y= 2.0;
+		//cloud.points[i].z = 2.0;
+		cloud.points[i].z = 1024 * rand() / (RAND_MAX + 1.0f);
 	}
 
 	// Set a few outliers
@@ -52,11 +133,8 @@ int main(int argc, char** argv) {
 
 	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
 	pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-	// Create the segmentation object
 	pcl::SACSegmentation<pcl::PointXYZ> seg;
-	// Optional
 	seg.setOptimizeCoefficients(true);
-	// Mandatory
 	seg.setModelType(pcl::SACMODEL_PLANE);
 	seg.setMethodType(pcl::SAC_RANSAC);
 	seg.setDistanceThreshold(0.01);
@@ -80,12 +158,5 @@ int main(int argc, char** argv) {
 				<< cloud.points[inliers->indices[i]].y << " "
 				<< cloud.points[inliers->indices[i]].z << std::endl;
 
-	//pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
-	//viewer.showCloud(cloud.Ptr,"kinect cloud");
-	   //while (!viewer.wasStopped())
-	   //{
-
-	   //}
-
 	return (0);
-}
+}*/
